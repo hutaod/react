9a53d605a2f07f7ed8b190d05a9ca475f71c2548
hutao1 <hutao@akulaku.com>
2020-04-24 18:40:44 +0800

    Update modules.js

--- scripts/rollup/modules.js
+++ scripts/rollup/modules.js
@@ -1,3 +1,10 @@
 'use strict'
 
 const forks = require('./forks')
+const { UMD_DEV, UMD_PROD, UMD_PROFILING } = require('./bundles').bundleTypes
+
+const HAS_NO_SIDE_EFFECTS_ON_IMPORT = false;
+
+const importSideEffects = Object.freeze({
+  'prop-types/checkPropTypes': HAS_NO_SIDE_EFFECTS_ON_IMPORT,
+})
